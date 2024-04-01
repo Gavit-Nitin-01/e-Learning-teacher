8d765d521185d079a40a4536576651d9d8635446
Gavit-Nitin-01 <gavitnitinratanbhai@gmail.com>
2024-04-01 12:39:15 +0530

    Change design

--- src/Components/ForgotPass/ForgotPass.jsx
+++ src/Components/ForgotPass/ForgotPass.jsx
@@ -3,16 +3,17 @@ import { Link } from 'react-router-dom'
 import { toast } from 'react-toastify';
 
 export default function ForgotPass() {
-    const sendLink = (e) =>{
+    const sendLink = (e) => {
         e.preventDefault();
         toast.info('Reset Password Link Send', {
             position: "top-right",
             autoClose: 2000,
             closeOnClick: true,
             theme: "colored",
-            });
+        });
+
     }
-    
+
     return (
         <>
             <div className="bg-light py-3 py-md-5">
@@ -24,7 +25,7 @@ export default function ForgotPass() {
                                     <div className="col-12">
                                         <div className="text-center">
                                             <a href="#!">
-                                                <img src="images/forgotpass.png" alt="forgot Password" width="157" height="157"/>
+                                                <img src="images/forgotpass.png" alt="forgot Password" width="157" height="157" />
                                             </a>
                                         </div>
                                     </div>
@@ -42,7 +43,7 @@ export default function ForgotPass() {
                                                         <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
                                                     </svg>
                                                 </span>
-                                                <input type="email" className="form-control" name="email" id="email" required/>
+                                                <input type="email" className="form-control" name="email" id="email" required />
                                             </div>
                                         </div>
                                         <div className="col-12">
@@ -54,11 +55,11 @@ export default function ForgotPass() {
                                 </form>
                                 <div className="row">
                                     <div className="col-12">
-                                        <hr className="mt-5 mb-4 border-secondary-subtle"/>
-                                            <div className="d-flex gap-4 justify-content-center">
-                                                <Link to="/" className="link-secondary text-decoration-none">Log In</Link>
-                                                <Link to="/signup" className="link-secondary text-decoration-none">Register</Link>
-                                            </div>
+                                        <hr className="mt-5 mb-4 border-secondary-subtle" />
+                                        <div className="d-flex gap-4 justify-content-center">
+                                            <Link to="/" className="link-secondary text-decoration-none">Log In</Link>
+                                            <Link to="/signup" className="link-secondary text-decoration-none">Register</Link>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
