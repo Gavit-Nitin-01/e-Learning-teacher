84bb4d36378a512d5cbdca2c605667cc1f898c46
Gavit-Nitin-01 <gavitnitinratanbhai@gmail.com>
2024-04-05 15:01:02 +0530

    upload video

--- src/Components/MyActivity/Uploadvideo/Uploadvideo.jsx
+++ src/Components/MyActivity/Uploadvideo/Uploadvideo.jsx
@@ -1,36 +1,58 @@
-import React from 'react'
-import './Uploadvideo.css'
+import React from "react";
+import "./Uploadvideo.css";
 export default function Uploadvideo() {
-    
   return (
-    
     <div>
-
-<div class="frame">
-	<div class="center">
-		<div class="title">
-			<h1> upload video</h1>
-		</div>
-
-		<div class="dropzone">
-			<img src="http://100dayscss.com/codepen/upload.svg" class="upload-icon" />
-			<input type="file" class="upload-input"  />
-		</div>
-
-		<button type="button" class="bttn" name="uploadbutton">Upload file</button>
-
-        <div className='Vinfo'>
-        <label for="vinfo">Video info</label><br></br>
-        <input type="text" id="vinfo" name="vinfo" placeholder='Video Title'/> 
+      <h2 className="heading">UPLOAD VIDEO</h2>
+      <div className="containerr ">
+        <div className="col-md-6">
+          <label for="inputState">videos</label>
+          <select id="inputState" class="form-control">
+            <option selected>Choose...</option>
+            <option>...</option>
+            <option>...</option>
+            <option>...</option>
+            <option>...</option>
+            <option>...</option>
+          </select>
         </div>
 
-        <div className='discription'>
-        <label for="discribe">Discription</label><br></br>
-        <textarea id="discribe" name="discribe" rows="3" cols="40"></textarea> 
+        <div className="Vdoinfo">
+          <label for="vinfo">Video title</label>
+          <br></br>
+          <input
+            type="text"
+            id="vinfo"
+            name="vinfo"
+            placeholder="Video Title"
+          />
         </div>
 
-	</div>
-</div>
+        <div class="discription">
+          <label for="discribe">Discription</label>
+          <br></br>
+          <textarea
+            id="discribe"
+            name="discribe"
+            rows="4"
+            cols="70"
+            placeholder="Discription"
+          ></textarea>
+        </div>
+
+        <div className="Vdochoose">
+          <div>choose video</div>
+          <div>
+            <input type="file" class="form-control-file" id="Vdoupload" />
+          </div>
+        </div>
+
+        
+          <button type="submit" className="uploadbtn">
+            Upload
+          </button>
+        
+      </div>
     </div>
-  )
+  );
 }
