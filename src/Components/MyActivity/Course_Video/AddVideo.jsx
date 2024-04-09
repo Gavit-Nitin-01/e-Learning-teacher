03c4e4253c12f972d37e27defb99d51bcd57684f
Gavit-Nitin-01 <gavitnitinratanbhai@gmail.com>
2024-04-09 15:08:56 +0530

    add video update

--- src/Components/MyActivity/Course_Video/AddVideo.jsx
+++ src/Components/MyActivity/Course_Video/AddVideo.jsx
@@ -8,7 +8,7 @@ export default function AddVideo() {
                 Add Video
             </button>
 
-            <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
@@ -21,11 +21,11 @@ export default function AddVideo() {
                                 <input type="email" className="form-control" id="title" placeholder="Enter Video Title" />
                             </div>
                             <div className="mb-3">
-                                <label htmoFor="exampleFormControlTextarea1" className="form-label">Description</label>
+                                <label  className="form-label">Description</label>
                                 <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder='Enter Short Description'></textarea>
                             </div>
                             <div className="mb-3">
-                                <label htmoFor="formFileDisabled" className="form-label">Uplad Video</label>
+                                <label  className="form-label">Uplad Video</label>
                                 <input className="form-control" type="file" accept='video/*' id="formFileDisabled" />
                             </div>
                         </div>
